1c25e95bce120eb21b2674a8b94b7f32f5159bfd
Danny Burrows <dannyburrows@protonmail.com>
2023-04-02 15:49:25 +0100

    Platform detection on import

--- rotatescreen/__init__.py
+++ rotatescreen/__init__.py
@@ -1,2 +1,9 @@
-from .display import *
-from .mac_display import *
+import platform
+
+system = platform.system()
+if system == "Windows":
+    from .display import *
+elif system == "Darwin":
+    from .mac_display import *
+else:
+    raise Exception(f"ERROR: Platform '{system}' is not yet supported!")
