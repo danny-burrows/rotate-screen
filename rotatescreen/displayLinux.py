6d141070b7c6f5c544438c816ba829d2d7b1f308
Danny Burrows <dannyburrows@protonmail.com>
2023-04-03 18:14:09 +0100

    Initial xlib/randr implementation

--- rotatescreen/displayLinux.py
+++ rotatescreen/displayLinux.py
@@ -1,21 +1,110 @@
-from __future__ import print_function
+from Xlib import display
+from Xlib.ext import randr 
+from typing import Dict, List, Tuple
 
-print("Linux Detected!")
+# TODO: Is having these at the global level ok?
+d = display.Display()
+# Create an X display and get the root window
+root = d.screen().root
+# Get the resources for the root window
+res = root.xrandr_get_screen_resources()
 
-from Xlib import X, display
-from Xlib.ext import randr
-from Xlib.error import DisplayNameError
 
-try:
-    d = display.Display()
-except DisplayNameError:
-    print("Error getting display.")
-    exit()
+class Display:
 
-s = d.screen()
-window = s.root.create_window(0, 0, 1, 1, 1, s.root_depth)
+    def __init__(self, output_id, crtc_id):
+        self.output_id = output_id
+        self.crtc_id = crtc_id
 
-res = randr.get_screen_resources(window)
-for mode in res.modes:
-    w, h = mode.width, mode.height
-    print("Width: {}, height: {}".format(w, h))
+    def __repr__(self):
+        return f"<'{self.device_description[0]}' object>"
+
+    def rotate_to(self, degrees: int) -> None:
+        if degrees == 90:
+            rotation_val = randr.Rotate_90
+        elif degrees == 180:
+            rotation_val = randr.Rotate_180
+        elif degrees == 270:
+            rotation_val = randr.Rotate_270
+        elif degrees == 0:
+            rotation_val = randr.Rotate_0
+        else:
+            raise ValueError("Display can only be rotated to 0, 90, 180, or 270 degrees.")
+        
+        # Get the CRTC's current mode and position
+        crtc = d.xrandr_get_crtc_info(self.crtc_id, res.config_timestamp)
+        x, y, mode = crtc.x, crtc.y, crtc.mode
+        
+        # Rotate the CRTC 90 degrees clockwise
+        d.xrandr_set_crtc_config(    
+            crtc=self.crtc_id,
+            config_timestamp=res.config_timestamp,
+            x=x,
+            y=y,
+            mode=mode,
+            rotation=rotation_val,
+            outputs=crtc.outputs
+        )
+
+    def set_landscape(self) -> None:
+        self.rotate_to(0)
+
+    def set_landscape_flipped(self) -> None:
+        self.rotate_to(180)
+
+    def set_portrait(self) -> None:
+        self.rotate_to(90)
+
+    def set_portrait_flipped(self) -> None:
+        self.rotate_to(270)
+
+    @property
+    def current_orientation(self) -> int:
+        # Get the CRTC's current mode information
+        mode_info = d.xrandr_get_crtc_info(self.crtc_id, res.config_timestamp)
+        
+        # Get the CRTC's current rotation
+        rotation = mode_info.rotation
+        if rotation == randr.Rotate_0:
+            return 0
+        elif rotation == randr.Rotate_90:
+            return 90
+        elif rotation == randr.Rotate_180:
+            return 180
+        elif rotation == randr.Rotate_270:
+            return 270
+
+    @property
+    def is_primary(self) -> bool:
+        # Create an X display and get the root window
+        root = d.screen().root
+
+        # Get the primary output
+        primary_output = root.xrandr_get_output_primary().output
+        
+        return self.output_id == primary_output
+
+    @property
+    def device_description(self) -> Tuple[str, str]:
+        output_info = d.xrandr_get_output_info(self.output_id, res.config_timestamp)    
+        return output_info.name, self.crtc_id
+
+
+def get_displays() -> List[Display]:
+    displays = []
+    for output_id in res.outputs:        
+        output_info = d.xrandr_get_output_info(output_id, res.config_timestamp)    
+        if crtc_id := output_info.crtc:
+            displays.append(Display(output_id, crtc_id))
+    return displays
+
+
+def get_primary_display() -> Display | None:
+    for display in get_displays():
+        if display.is_primary:
+            return display
+
+
+def get_secondary_displays() -> List[Display]:
+    displays = [display for display in get_displays() if not display.is_primary]
+    return displays
