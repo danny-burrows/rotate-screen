94713a4eba8c85e5c5e4339bb6f29f117f317760
Danny Burrows <dannyburrows@protonmail.com>
2023-04-03 22:11:23 +0100

    Rename files & platform detection

--- rotatescreen/__init__.py
+++ rotatescreen/__init__.py
@@ -1,4 +1,9 @@
-try:
-    from .displayWindows import *
-except ImportError:
-    from .displayLinux import *
+import platform
+
+system = platform.system()
+if system == "Windows":
+    from .display_windows import *
+elif system == "Linux":
+    from .display_linux import *
+else:
+    raise Exception(f"ERROR: Platform '{system}' is not yet supported!")
